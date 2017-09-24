fe22a04a46cd9f2d1635f7083de556d864ff01d9
Yoshimasa Sakuragi <ysrhsp@outlook.com>
2017-09-24 17:45:18 +0900

    initial commit

---
+++ FluctuateViewController/FluctuateViewController.h
@@ -0,0 +1,19 @@
+//
+//  FluctuateViewController.h
+//  FluctuateViewController
+//
+//  Created by Yoshimasa Sakuragi on 2017/09/24.
+//  Copyright © 2017年 Yoshimasa Sakuragi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FluctuateViewController.
+FOUNDATION_EXPORT double FluctuateViewControllerVersionNumber;
+
+//! Project version string for FluctuateViewController.
+FOUNDATION_EXPORT const unsigned char FluctuateViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluctuateViewController/PublicHeader.h>
+
+
